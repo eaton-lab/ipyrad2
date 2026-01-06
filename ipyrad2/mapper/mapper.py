e93178d011c1faff3d1a099980c55e5b4e3de762
deren <de2356@columbia.edu>
2026-01-06 15:32:51 -0500

    support imap, max softclip, max edit_dist, and write more stats

--- ipyrad2/mapper/mapper.py
+++ ipyrad2/mapper/mapper.py
@@ -46,6 +46,7 @@ def map_filter_sort_mark_pairs(sname: str, fastqs: Tuple[Path, Path], reference:
     # paths
     # logger.debug(f"starting mapping of {sname}: {fastqs}")
     out_bam = outdir / f"{sname}.filtered.bam"
+    out_bam_tmp = outdir / f"{sname}.filtered.bam.tmp"
     bam_namesort = outdir / "tmpdir" / f"{sname}.tmp.namesort.bam"
     bam_fixmate = outdir / "tmpdir" / f"{sname}.tmp.fixmate.bam"
     bam_coordsort = outdir / "tmpdir" / f"{sname}.tmp.coordsort.bam"
@@ -155,27 +156,35 @@ def map_filter_sort_mark_pairs(sname: str, fastqs: Tuple[Path, Path], reference:
         "-e", '((flag&4)==0) && ((flag&8)==0) && (rnext=="=" || rnext==rname) && (tlen>=-2000 && tlen<=2000)',
         "--save-counts", str(tmp_stats3),
         "--write-index",
-        "-o", str(out_bam),
+        "-o", str(out_tmp_bam),
     ]
     run_pipeline([cmd7, cmd8])
     logger.debug(f"finished mapping: {sname}")
+
+    # rename files to finished name (protects for restarting w/o -f)
+    os.replace(out_bam_tmp, out_bam)
+    os.replace(out_bam_tmp.with_suffix(out_bam_tmp.suffix + ".csi"), out_bam.with_suffix(out_bam.suffix + ".csi"))
+
+    # remove tmp files
     bam_markdup.unlink()
     bam_markdup.with_suffix(bam_markdup.suffix + '.csi').unlink()
     return out_bam
 
 
-def map_filter_sort_pairs(sname: str, fastqs: Tuple[Path, Path], reference: Path, min_map_q: int, outdir: Path, threads: int, **kwargs) -> Path:
+def map_filter_sort_pairs(sname: str, fastqs: Tuple[Path, Path], reference: Path, min_map_q: int, max_soft_clip: int, max_edit_dist: int, outdir: Path, threads: int, **kwargs) -> Path:
     """Map reads to the reference to get a sorted bam.
 
     This pipeline is for PE data w/o duplication marking information.
     """
     # paths
     out_bam = outdir / f"{sname}.filtered.bam"
-    # tmp_bam = outdir / f"{sname}.bam.tmp"
+    out_bam_tmp = outdir / f"{sname}.filtered.bam.tmp"
     tmp_prefix = outdir / "tmpdir" / f"{sname}.tmp.pre"
     tmp_stats1 = outdir / "tmpdir" / f"{sname}.tmp.stats1.json"
     tmp_stats2 = outdir / "tmpdir" / f"{sname}.tmp.stats2.json"
     tmp_stats3 = outdir / "tmpdir" / f"{sname}.tmp.stats3.json"
+    tmp_stats4 = outdir / "tmpdir" / f"{sname}.tmp.stats4.json"
+    tmp_stats5 = outdir / "tmpdir" / f"{sname}.tmp.stats5.json"
 
     # Split threads between BWA and samtools
     bwa_threads = max(1, int(threads * 0.75))
@@ -184,7 +193,7 @@ def map_filter_sort_pairs(sname: str, fastqs: Tuple[Path, Path], reference: Path
     # mapping command
     cmd1 = [
         BIN_BWA, "mem",
-        # "-Y",                     # soft-clip supplementary. Wouldn't hurt, but not necessary.
+        "-Y",                     # soft-clip supplementary. Wouldn't hurt, but not necessary.
         "-T", "20",               # minimum score to output (default=30). Keep lower scores for now, we filter on MAPQ later.
         "-R", f"@RG\\tID:{sname}\\tSM:{sname}",  # store sample names in bam.
         "-K", "50000000",         # stable chunk size. Improves repeatability.
@@ -223,52 +232,114 @@ def map_filter_sort_pairs(sname: str, fastqs: Tuple[Path, Path], reference: Path
         "-o", "-",
     ]
 
-    # coordinate sort
+    # allow at most this many soft-clipped bases
     cmd5 = [
+        BIN_SAMTOOLS, "view",
+        "-b", "-u",
+        "-e", f"sclen <= {max_soft_clip}",
+        # "-e", f"sclen <= {max_soft_clip}",
+        "--save-counts", str(tmp_stats4),
+        "-o", "-",
+    ]
+
+    # allow at most this many changes relative to the reference
+    cmd6 = [
+        BIN_SAMTOOLS, "view",
+        "-b", "-u",
+        "-e", f"[NM] < {max_edit_dist}",
+        "--save-counts", str(tmp_stats5),
+        "-o", "-",
+    ]
+
+    # coordinate sort
+    cmd7 = [
         BIN_SAMTOOLS, "sort",
         "-m", "256M",                # per-thread memory
         "-T", str(tmp_prefix),
         "-@", str(sort_threads),
         "--write-index",
         "-O", "bam",                 # explicity ask for bam format to be safe
-        "-o", str(out_bam),
+        "-o", str(out_bam_tmp),
         "-",
     ]
-    cmds = [cmd1, cmd2, cmd3, cmd4, cmd5]
+    cmds = [cmd1, cmd2, cmd3, cmd4, cmd5, cmd6, cmd7]
     run_pipeline(cmds)
+
+    # rename files to finished name (protects for restarting w/o -f)
+    os.replace(out_bam_tmp, out_bam)
+    os.replace(out_bam_tmp.with_suffix(out_bam_tmp.suffix + ".csi"), out_bam.with_suffix(out_bam.suffix + ".csi"))
+
     logger.info(f"finished mapping: {sname}")
     return out_bam
 
 
 def concat_tech_reps_into_tmpdir(imap: Path, tmpdir: Path, fastq_dict: Dict[str, Tuple[Path, Path]]) -> Dict[str, Path]:
-    """Return fastq_dict pointing to updated concat paths in tmpdir"""
-    # parse the population file
-    df = pd.read_csv(imap, header=None, sep=r"\s+")
+    """Return fastq_dict revised by an imap file.
 
+    The imap file is a whitespace delimited table of sample names to
+    include in the mapping step, and optionally a second column with
+    new names. The names in the imap file should match the names that
+    will be parsed by the CLI name-delimiter, i.e., not the file names
+    with .trimmed.fastq.gz, etc.
+
+    The imap file can serve three functions here: (1) to subsample
+    which samples to process from a glob of potentially many files;
+    (2) to rename samples, in which case they are written to
+    MAPPED/{newname}.filtered.bam; and (3) to combine technical
+    replicates by assigning two or more samples to the same name.
+
+    IMAP file examples
+    ------------------
+    # one column: only these samples will be processed from glob.
+    sample_1A
+    sample_2A
+    sample_3A
+
+    # two columns: samples with a column 2 value will be renamed to it.
+    sample_1A   sample_A
+    sample_2B   sample_B
+    sample_3C   sample_C
+
+    # two columns: samples with the same column 2 value will be combined and renamed.
+    sample_1A   sample_A
+    sample_2A   sample_A
+    sample_3B   sample_B
+    """
     # fill pdict and warn if names don't match any samples.
     snames = set(fastq_dict)
-    pop2tups = defaultdict(list)
-    pop2snames = defaultdict(list)
+    pname_to_path_tuples = defaultdict(list)
+    pname_to_snamelist = defaultdict(list)
     warn_list = []
-    for idx in df.index:
-        sname, pname, *_ = df.loc[idx]
-        if sname in snames:
-            pop2tups[pname].append(fastq_dict.pop(sname))
-            pop2snames[pname].append(sname)
-        else:
-            warn_list.append(sname)
+
+    # parse the population file.
+    with open(imap, 'r') as infile:
+        for line in infile:
+            if not line.strip():
+                continue
+            sname, *data = line.strip().split()
+            if data:
+                pname = data[0]
+            else:
+                pname = sname
+
+            # if imap sname is in fastq snames then store it and its file paths
+            if sname in snames:
+                pname_to_path_tuples[pname].append(fastq_dict.pop(sname))
+                pname_to_snamelist[pname].append(sname)
+            else:
+                warn_list.append(sname)
+
+    # warn user about mismatches names and bail if no names were selected
     if warn_list:
         logger.warning(f"One or more names in imap file did not match sample names and will be skipped: {' '.join(warn_list)}")
-
-    # return original dict if nothing to merge/rename
-    if not pop2snames:
-        return fastq_dict
+    if not pname_to_snamelist:
+        raise IPyradError("No samples in imap file match parsed sample names. Revise imap file or name parsing args.")
 
     # report to logger
-    logger.info(f"merging/renaming samples according to imap file: {imap}")
-    maxlen = max(len(i) for i in pop2snames)
-    for pname, tups in pop2tups.items():
-        snames = pop2snames[pname]
+    logger.info(f"subselecting, renaming, or merging {len(fastq_dict)} samples into {len(pname_to_snamelist)} samples according to: {imap.name}")
+    maxlen = max(len(i) for i in pname_to_snamelist)
+    for pname, tups in pname_to_path_tuples.items():
+        snames = pname_to_snamelist[pname]
         logger.info(f"{pname}{' ' * (maxlen - len(pname))} <- {' + '.join(snames)}")
 
         # renaming, do not run pipe
@@ -326,6 +397,16 @@ def count_mapped_reads(sname: str, outdir: Path) -> int:
     with s3.open('r') as indata:
         d3 = json.loads(indata.read())
 
+    # tmp stats4.json contains filter to remove max soft clip
+    s4 = outdir / "tmpdir" / f"{sname}.tmp.stats4.json"
+    with s4.open('r') as indata:
+        d4 = json.loads(indata.read())
+
+    # tmp stats5.json contains filter to remove max edit dist
+    s5 = outdir / "tmpdir" / f"{sname}.tmp.stats5.json"
+    with s5.open('r') as indata:
+        d5 = json.loads(indata.read())
+
     # get mean, std of mapq
     bam_file = outdir / f"{sname}.filtered.bam"
     cmd1 = [BIN_SAMTOOLS, "view", str(bam_file)]
@@ -343,12 +424,14 @@ def count_mapped_reads(sname: str, outdir: Path) -> int:
 
     data = {
         "nreads_processed": d1["records_processed"],
-        "nreads_filtered_by_not_primary": d1["records_filter_rejected"],
-        "nreads_filtered_by_duplicates": dd["records_filter_rejected"],
-        "nreads_filtered_by_min_mapq": d2["records_filter_rejected"],
-        "nreads_filtered_by_bad_pairing": d3["records_filter_rejected"],
         "nreads_passed_filters": d3["records_filter_accepted"],
-        "proportion_reads_passed_filters": float(d3["records_filter_accepted"] / d1["records_processed"]),
+        "proportion_passed_filters": float(d5["records_filter_accepted"] / d1["records_processed"]),
+        "filtered_by_not_primary": d1["records_filter_rejected"],
+        "filtered_by_duplicates": dd["records_filter_rejected"],
+        "filtered_by_min_mapq": d2["records_filter_rejected"],
+        "filtered_by_bad_pairing": d3["records_filter_rejected"],
+        "filtered_by_max_soft_clip": d4["records_filter_rejected"],
+        "filtered_by_max_edit_dist": d5["records_filter_rejected"],
         "mapq_mean_after_filters": float(mean_mapq),
         "mapq_median_after_filters": float(median_mapq),
         "mapq_stdev_after_filters": float(stdev_mapq),
@@ -393,6 +476,8 @@ def run_mapper(
     reference: Path,
     imap: Path,
     min_map_q: int,
+    max_soft_clip: int,
+    max_edit_dist: int,
     cores: int,
     threads: int,
     force: bool,
@@ -475,6 +560,8 @@ def run_mapper(
             outdir=outdir,
             reference=reference,
             min_map_q=min_map_q,
+            max_soft_clip=max_soft_clip,
+            max_edit_dist=max_edit_dist,
             mark_dups_by_umis=mark_dups_by_umis,
             threads=threads,
         )
@@ -509,12 +596,14 @@ def run_mapper(
         outstats,
         formatters={
             "nreads_processed": lambda x: f"{int(x)}",
-            "nreads_filtered_by_not_primary": lambda x: f"{int(x)}",
-            "nreads_filtered_by_duplicates": lambda x: f"{int(x)}",
-            "nreads_filtered_by_min_mapq": lambda x: f"{int(x)}",
-            "nreads_filtered_by_bad_pairing": lambda x: f"{int(x)}",
             "nreads_passed_filters": lambda x: f"{int(x)}",
-            "proportion_reads_passed_filters": lambda x: f"{x:.3f}",
+            "proportion_passed_filters": lambda x: f"{x:.3f}",
+            "filtered_by_not_primary": lambda x: f"{int(x)}",
+            "filtered_by_duplicates": lambda x: f"{int(x)}",
+            "filtered_by_min_mapq": lambda x: f"{int(x)}",
+            "filtered_by_bad_pairing": lambda x: f"{int(x)}",
+            "filtered_by_max_soft_clip": lambda x: f"{int(x)}",
+            "filtered_by_max_edit_dist": lambda x: f"{int(x)}",
             "mapq_mean_after_filters": lambda x: f"{x:.3f}",
             "mapq_median_after_filters": lambda x: f"{x:.3f}",
             "mapq_stdev_after_filters": lambda x: f"{x:.3f}",
