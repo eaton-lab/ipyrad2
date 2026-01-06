c7e3e2c1f2a18fdf2f8e309f2104d851f58d19d6
deren <de2356@columbia.edu>
2026-01-06 15:29:04 -0500

    added/renamed several trim args

--- ipyrad2/cli/cli_trim.py
+++ ipyrad2/cli/cli_trim.py
@@ -33,31 +33,43 @@ def _setup_trim_subparser(subparsers: argparse._SubParsersAction, header: str = 
     )
     tool.add_argument(
         "-o", "--out", metavar="Path", type=Path, default="./TRIMMED",
-        help="Directory to write results. Created if it doesn't exist. [default=TRIMMED]",
+        help="Directory to write results. Created if it doesn't exist. [%(default)s]",
+    )
+    tool.add_argument(
+        "-u", "--max-unqualified-percent", metavar="int", type=int, default=15,
+        help="Max percent of unqualified bases in a read. [%(default)s]",
     )
     tool.add_argument(
         "-q", "--min-quality", metavar="int", type=int, default=20,
-        help="Minimum base quality score. [default=20]",
+        help="Min base quality. The threshold Q score below which a site is unqualified (see -). [%(default)s]",
     )
     tool.add_argument(
-        "-n", "--max-low-quality-bases", metavar="int", type=int, default=5,
-        help="Maximum number of low quality bases. [default=5]",
+        "-M", "--min-mean-window-quality", metavar="int", type=int, default=30,
+        help="Min mean base quality when trimming in sliding windows from 5' and 3'. [%(default)s]",
     )
     tool.add_argument(
-        "-m", "--min-trimmed-length", metavar="int", type=int, default=35,
-        help="Minimum length of retained trimmed reads. [default=35]",
+        "-W", "--cut-window-size", metavar="int", type=int, default=5,
+        help="Size of sliding windows when trimming if mean quality <= -m. [%(default)s]",
+    )
+    tool.add_argument(
+        "-n", "--max-ns", metavar="int", type=int, default=5,
+        help="Maximum number of uncalled (N) bases. [%(default)s]",
+    )
+    tool.add_argument(
+        "-e", "--min-trimmed-length", metavar="int", type=int, default=35,
+        help="Minimum length of retained trimmed reads. [%(default)s]",
     )
     tool.add_argument(
         "-x", "--max-reads", metavar="int", type=intlike, default=None,
-        help="Maximum number of reads per file (useful for testing or to normalize inputs). [default=None]"
+        help="Maximum number of reads per file (useful for testing or to normalize inputs). [%(default)s]",
     )
     tool.add_argument(
         "-r", "--restriction-overhangs", metavar="str", nargs=2, type=str,
-        help="Restriction overhangs (overrides inferred REs). [default=(None, None)]",
+        help="Restriction overhangs (overrides inferred REs). [default=None None]",
     )
     tool.add_argument(
         "-p", "--phred-qscore-offset", metavar="int", type=int, default=33,
-        help="Q score offset (to accommodate legacy data). [default=33]",
+        help="Q score offset (to accommodate legacy data). [%(default)s]",
     )
     tool.add_argument(
         "-k", "--max-reads-kmer", metavar="int", type=intlike, default=500_000,
@@ -77,11 +89,11 @@ def _setup_trim_subparser(subparsers: argparse._SubParsersAction, header: str = 
     )
     tool.add_argument(
         "-c", "--cores", metavar="int", type=int, default=6,
-        help="Max number of cores to use. [default=6]",
+        help="Max number of cores to use. [%(default)s]",
     )
     tool.add_argument(
         "-t", "--threads", metavar="int", type=int, default=3,
-        help="Run c/t multi-threaded jobs concurrently. Larger -t reduces RAM and I/O. [default=3]",
+        help="Run c/t multi-threaded jobs concurrently. Larger -t reduces RAM and I/O. [%(default)s]",
     )
     tool.add_argument(
         "-f", "--force", action="store_true",
@@ -89,19 +101,19 @@ def _setup_trim_subparser(subparsers: argparse._SubParsersAction, header: str = 
     )
     tool.add_argument(
         "-dx", "--delim-str", metavar="str", type=str, default=None,
-        help="Set delim substring 'dx' to override name parsing from files. [default=None]"
+        help="Set delim substring 'dx' to override name parsing from files. [%(default)s]"
     )
     tool.add_argument(
         "-di", "--delim-idx", metavar="int", type=int, default=1,
-        help="Set delim index. Extracts substring left of the 'di'-th 'dx' in filename. [default=1]",
+        help="Set delim index. Extracts substring left of the 'di'-th 'dx' in filename. [%(default)s]",
     )
     # TODO
     tool.add_argument(
         "-s", "--suffix", metavar="str", type=str,
-        help=r"Add a suffix to sample names (output: {name}{suffix}.trimmed._R*.fastq.gz). [default=None]",
+        help=r"Add a suffix to sample names (output: {name}{suffix}.trimmed._R*.fastq.gz). [%(default)s]",
     )
     tool.add_argument(
-        "-u", "--umi-tag-in-i5", action="store_true",
+        "-U", "--umi-tag-in-i5", action="store_true",
         help="Move i5 index into read name. Use if i5 contains UMIs for marking PCR duplicates.",
     )
     tool.add_argument(
